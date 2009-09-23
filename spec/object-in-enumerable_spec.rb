52b1a2e22555d2f435ed7666655b4f1f202b8cfc
Patrick Byrne <code@patrickbyrne.net>
2009-09-23 12:03:04 -0500

    added gemspec and additional tests; added readme

--- spec/object-in-enumerable_spec.rb
+++ spec/object-in-enumerable_spec.rb
@@ -7,7 +7,7 @@ describe Object do
       @string = "abcdefg"
     end
     
-    it "should know when it is in the given enumerable" do
+    it "should know when it is in the given array" do
       1.in?(@enum).should be_true
       "B".in?(@enum).should be_true
       Date.today.in?(@enum).should be_true
@@ -15,7 +15,7 @@ describe Object do
       nil.in?(@enum).should be_true
     end
     
-    it "should know when it is not in the given enumerable" do
+    it "should know when it is not in the given array" do
       2.in?(@enum).should be_false
       "C".in?(@enum).should be_false
       Date.new(2000, 01, 01).in?(@enum).should be_false
@@ -25,11 +25,13 @@ describe Object do
     it "should know when it is in a non-enumerable object" do
       "a".in?("abc").should be_true
       "ABC".in?("ABCDEFG").should be_true
+      1.in?(1..5).should be_true
     end
     
     it "should know when it is not in a non-enumerable object" do
       "d".in?("abc").should be_false
       "Z".in?("xyz").should be_false
+      1.in?(5..10).should be_false
     end
     
     it "should return nil if the given enumerable doesn't respond to include?" do
